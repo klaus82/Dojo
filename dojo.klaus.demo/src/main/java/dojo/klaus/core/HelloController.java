98fa0fc0bf1981eac69e655c5bed1d63ea182155
Klaus82 <cla.klaus@yahoo.it>
2015-10-13 22:12:16 +0200

    Primo commit progetto test SPRING

---
+++ dojo.klaus.demo/src/main/java/dojo/klaus/core/HelloController.java
@@ -0,0 +1,18 @@
+package dojo.klaus.core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello(){
+		
+		return "Hello, World!";
+		
+	}
+	
+	
+
+}
