35c69797c8c8e5a5e333868396020b833d3926f2
Klaus82 <cla.klaus@yahoo.it>
2015-10-19 07:29:54 +0200

    First tutorial from Spring official website. Create a rest service master

--- dojo.klaus.demo/src/main/java/dojo/klaus/core/HelloController.java
+++ dojo.klaus.demo/src/main/java/dojo/klaus/core/HelloController.java
@@ -3,6 +3,7 @@ package dojo.klaus.core;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// Commento prova
 @RestController
 public class HelloController {
 	
---
+++ gs-rest-service-master/src/main/java/com/example/GsRestServiceMasterApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsRestServiceMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsRestServiceMasterApplication.class, args);
+    }
+}
